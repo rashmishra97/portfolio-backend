4f6cd7f1db4d4d8e0a713f546d9c2f9e7318b7d7
rashmishra97 <rashmishra0@gmail.com>
2026-02-04 13:41:59 +0530

    final changes

--- models/message.js
+++ models/message.js
@@ -12,18 +12,3 @@ const messageSchema = new mongoose.Schema(
 
 export default mongoose.model("Message", messageSchema);
 
-
-
-
-// import mongoose from "mongoose";
-
-// const MessageSchema = new mongoose.Schema(
-//   {
-//     name: String,
-//     message: String
-//   },
-//   { timestamps: true }
-// );
-
-// export default mongoose.model("Message", MessageSchema);
-
--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -17,22 +17,3 @@ router.get("/", async (req, res) => {
 
 export default router;
 
-
-
-
-// import express from "express";
-// import Message from "../models/message.js";
-
-// const router = express.Router();
-
-// router.post("/", async (req, res) => {
-//   const data = await Message.create(req.body);
-//   res.status(201).json(data);
-// });
-
-// router.get("/", async (req, res) => {
-//   const data = await Message.find().sort({ createdAt: -1 });
-//   res.json(data);
-// });
-
-// export default router;
